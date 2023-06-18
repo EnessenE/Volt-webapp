f792f817d36fa81990f1ab3e7b1c5b34d0810bec
Enes <9263360+EnessenE@users.noreply.github.com>
2023-06-18 20:22:50 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    serverwebsocket: "wss://localhost:5001/hubs/v1/chat",
-    apiServer: "https://localhost:5001"
+    serverwebsocket: "wss://volt.api.reasulus.nl/hubs/v1/chat",
+    apiServer: "https://volt.api.reasulus.nl"
 };
